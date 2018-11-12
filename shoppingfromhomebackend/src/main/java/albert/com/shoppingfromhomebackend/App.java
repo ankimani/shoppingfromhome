c9f43449c2ffd15109e96444fef3ce6872cc1d38
Albert Njane <albertnjanek93@gmail.com>
2018-11-12 12:46:21 +0300

    PROJECT UPLOADED TO GITHUB SUCCESSFULLY

---
+++ shoppingfromhomebackend/src/main/java/albert/com/shoppingfromhomebackend/App.java
@@ -0,0 +1,13 @@
+package albert.com.shoppingfromhomebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
